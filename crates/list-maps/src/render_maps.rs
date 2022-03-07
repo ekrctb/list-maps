c86f514a58843dc6473151398c92b82440d1b999
ekrctb <ekrctb@gmail.com>
2022-03-07 22:45:16 +0900

    Render per-mod info

--- crates/list-maps/src/render_maps.rs
+++ crates/list-maps/src/render_maps.rs
@@ -4,8 +4,14 @@ Output beatmap summary to be used by the html/js interface.
 
 */
 
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufWriter, Write},
+    path::PathBuf,
+};
 
+use anyhow::Context;
 use bstr::BString;
 use osu_db_dump::{value::Ruleset, Db, Mods};
 use serde::{Deserialize, Deserializer};
@@ -26,6 +32,7 @@ pub struct Beatmap {
     pub beatmapset_id: u32,
     pub version: BString,
     pub hit_length: u32,
+    pub diff_approach: f32,
     pub diff_size: f32,
     pub playmode: Ruleset,
     pub approved: u8,
@@ -105,6 +112,10 @@ impl ModsIndex {
 
     pub fn index(&self, mods: Mods) -> usize {
         *self.index.get(&(mods & self.mask)).unwrap()
+    }
+
+    pub fn enumerate(&self) -> impl Iterator<Item = (usize, Mods)> + '_ {
+        self.mods.iter().copied().enumerate()
     }
 }
 
@@ -192,47 +203,78 @@ impl ScoreStatistics {
     }
 }
 
+#[allow(unused)]
 pub mod difficulty_attribs {
     // this is called "Aim" in DB.
     pub const STARS: u8 = 1;
+    // Can be calculated from nomod AR
     pub const AR: u8 = 7;
+    // Same as the nomod max combo.
     pub const MAX_COMBO: u8 = 9;
 }
 
-#[derive(Debug, Default, Clone)]
-pub struct DifficultyAttributes {
-    stars: f32,
-    approach_rate: f32,
-    max_combo: f32,
+pub struct DifficultyAttributeMap {
+    max_combo: HashMap<u32, f32>,
+    stars: HashMap<(u32, Mods), f32>,
+}
+
+impl DifficultyAttributeMap {
+    pub fn new() -> Self {
+        Self {
+            max_combo: HashMap::new(),
+            stars: HashMap::new(),
+        }
+    }
+
+    pub fn insert(&mut self, beatmap_id: u32, mods: Mods, attrib_id: u8, value: f32) -> bool {
+        if mods.is_empty() && attrib_id == difficulty_attribs::MAX_COMBO {
+            self.max_combo.insert(beatmap_id, value).is_none()
+        } else if attrib_id == difficulty_attribs::STARS {
+            self.stars.insert((beatmap_id, mods), value).is_none()
+        } else {
+            false
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.max_combo.len()
+    }
+
+    pub fn get_max_combo(&self, beatmap_id: u32) -> Option<f32> {
+        self.max_combo.get(&beatmap_id).copied()
+    }
+
+    pub fn get_stars(&self, beatmap_id: u32, mods: Mods) -> Option<f32> {
+        self.stars.get(&(beatmap_id, mods)).copied()
+    }
 }
 
 pub fn load_difficulty_attributes(
     super_opts: &super::Opts,
-) -> anyhow::Result<HashMap<(u32, Mods), DifficultyAttributes>> {
+) -> anyhow::Result<DifficultyAttributeMap> {
     eprintln!("Loading beatmap difficulty attributes...");
 
-    let mut diffs = HashMap::<(u32, Mods), DifficultyAttributes>::new();
+    let mut diffs = DifficultyAttributeMap::new();
+    let mut loaded_attrs = 0u64;
     let mut total_attrs = 0u64;
 
     for entry in super_opts.deserialize_iter(Db::BeatmapDifficultyAttribs)? {
         let entry: DifficultyAttribute = entry?;
+        total_attrs += 1;
+
         let value = match entry.value {
             None => continue,
             Some(v) => v,
         };
-        let diff = diffs.entry((entry.beatmap_id, entry.mods)).or_default();
 
-        match entry.attrib_id {
-            difficulty_attribs::STARS => diff.stars = value,
-            difficulty_attribs::AR => diff.approach_rate = value,
-            difficulty_attribs::MAX_COMBO => diff.max_combo = value,
-            _ => continue,
+        if diffs.insert(entry.beatmap_id, entry.mods, entry.attrib_id, value) {
+            loaded_attrs += 1;
         }
-        total_attrs += 1;
     }
 
     eprintln!(
-        "Loaded {} attributes of {} beatmaps",
+        "Loaded {}/{} attributes of {} beatmaps",
+        loaded_attrs,
         total_attrs,
         diffs.len()
     );
@@ -243,7 +285,8 @@ pub fn load_difficulty_attributes(
 pub struct BeatmapInfo<'a> {
     set: &'a BeatmapSet,
     map: Beatmap,
-    diff_nm: &'a DifficultyAttributes,
+    max_combo: f32,
+    nm_stars: f32,
     score_stats: Vec<ScoreStatistics>,
 }
 
@@ -260,6 +303,9 @@ pub struct Opts {
     #[clap(long = "mode", default_value = "2")]
     mode: Ruleset,
 
+    #[clap(long = "output-dir", about = "The output directory.")]
+    output_dir: Option<PathBuf>,
+
     #[clap(long = "no-convert")]
     no_convert: bool,
 
@@ -268,9 +314,6 @@ pub struct Opts {
         about = "Don't read the score database to collect score statistics."
     )]
     no_score_stats: bool,
-
-    #[clap(long = "verbose")]
-    verbose: bool,
 }
 
 impl Opts {
@@ -293,33 +336,32 @@ impl Opts {
 
             let set = match sets.get(&map.beatmapset_id) {
                 None => {
-                    if self.verbose {
-                        eprintln!(
-                            "{}/{}: no beatmapset info",
-                            map.beatmapset_id, map.beatmap_id
-                        );
-                    }
+                    eprintln!(
+                        "{}/{}: no beatmapset info",
+                        map.beatmapset_id, map.beatmap_id
+                    );
                     continue;
                 }
                 Some(set) => set,
             };
 
-            let diff_nm = match diffs.get(&(map.beatmap_id, Mods::empty())) {
-                None => {
-                    if self.verbose {
-                        eprintln!(
-                            "{}/{}: no difficulty info",
-                            map.beatmapset_id, map.beatmap_id,
-                        );
-                    }
+            let (max_combo, nm_stars) = match (
+                diffs.get_max_combo(map.beatmap_id),
+                diffs.get_stars(map.beatmap_id, Mods::empty()),
+            ) {
+                (Some(max_combo), Some(stars)) => (max_combo, stars),
+                _ => {
+                    eprintln!(
+                        "{}/{}: no difficulty info",
+                        map.beatmapset_id, map.beatmap_id,
+                    );
                     continue;
                 }
-                Some(diff) => diff,
             };
 
             map_count += 1;
 
-            if diff_nm.stars < self.min_stars {
+            if nm_stars < self.min_stars {
                 continue;
             }
 
@@ -328,15 +370,12 @@ impl Opts {
             let info = BeatmapInfo {
                 set,
                 map,
-                diff_nm,
+                max_combo,
+                nm_stars,
                 score_stats: vec![ScoreStatistics::default(); mods_info.diff_mods.len()],
             };
 
-            if self.no_score_stats {
-                self.display_beatmap_info(&mods_info, info)?;
-            } else {
-                filtered_maps.push(info);
-            }
+            filtered_maps.push(info);
         }
 
         eprintln!(
@@ -346,25 +385,57 @@ impl Opts {
 
         if !self.no_score_stats {
             self.collect_score_stats(super_opts, &mut filtered_maps)?;
+        }
 
-            filtered_maps.sort_by(|x, y| x.set.approved_date.cmp(&y.set.approved_date));
+        filtered_maps.sort_by(|x, y| x.set.approved_date.cmp(&y.set.approved_date));
 
-            for info in filtered_maps {
-                self.display_beatmap_info(&mods_info, info)?;
+        let mut summary_writer = self.open_output_file("summary.csv")?;
+        let mut mods_writers = mods_info
+            .diff_mods
+            .enumerate()
+            .map(|(_, mods)| self.open_output_file(&format!("mods-{}.csv", mods.bits())))
+            .collect::<Result<Vec<_>, _>>()?;
+
+        for info in &filtered_maps {
+            self.display_beatmap_info(&mut summary_writer, &mods_info, info)?;
+
+            for (i, mods) in mods_info.diff_mods.enumerate() {
+                let stars = match diffs.get_stars(info.map.beatmap_id, mods) {
+                    Some(diff) => diff,
+                    None => {
+                        eprintln!(
+                            "{}/{} {:?}: no difficulty info",
+                            info.set.beatmapset_id, info.map.beatmap_id, mods,
+                        );
+                        continue;
+                    }
+                };
+
+                self.display_mods_info(
+                    &mut mods_writers[i],
+                    info,
+                    mods,
+                    stars,
+                    info.score_stats[i],
+                )?;
             }
         }
 
         Ok(())
     }
 
-    fn display_beatmap_info(&self, mods_info: &ModsInfo, info: BeatmapInfo) -> anyhow::Result<()> {
-        let stat_total = info
-            .score_stats
-            .iter()
+    fn display_beatmap_info(
+        &self,
+        writer: &mut dyn Write,
+        mods_info: &ModsInfo,
+        info: &BeatmapInfo,
+    ) -> anyhow::Result<()> {
+        let stat_total = mods_info
+            .diff_mods
             .enumerate()
-            .filter(|(i, _)| !mods_info.diff_mods.mods[*i].contains(Mods::HALF_TIME))
-            .fold(ScoreStatistics::default(), |total, (_, stat)| {
-                total.merge(stat)
+            .filter(|(_, mods)| !mods.contains(Mods::HALF_TIME))
+            .fold(ScoreStatistics::default(), |total, (i, _)| {
+                total.merge(&info.score_stats[i])
             });
 
         let title_format = format!(
@@ -372,8 +443,8 @@ impl Opts {
             info.set.artist, info.set.title, info.map.version, info.set.creator
         );
 
-        println!(
-            "{approved_date:?},{set_id},{map_id},{status},{mode},{title:?},{hit_length},{nm_stars},{nm_max_combo},{nm_ar},{cs},{total_fc},{total_fc_flags}",
+        writeln!(writer,
+            "{approved_date:?},{set_id},{map_id},{status},{mode},{title:?},{hit_length},{nm_stars},{max_combo},{ar},{cs},{total_fc},{total_fc_flags}",
             approved_date = info.set.approved_date,
             set_id = info.map.beatmapset_id,
             map_id = info.map.beatmap_id,
@@ -381,13 +452,33 @@ impl Opts {
             mode = info.map.playmode.id(),
             title = title_format,
             hit_length = info.map.hit_length,
-            nm_stars = info.diff_nm.stars,
-            nm_max_combo = info.diff_nm.max_combo,
-            nm_ar = info.diff_nm.approach_rate,
+            nm_stars = info.nm_stars,
+            max_combo = info.max_combo,
+            ar = info.map.diff_approach,
             cs = info.map.diff_size,
             total_fc = stat_total.min_miss_or_fc_count(),
             total_fc_flags = stat_total.fc_flags,
-        );
+        )?;
+        Ok(())
+    }
+
+    fn display_mods_info(
+        &self,
+        writer: &mut dyn Write,
+        info: &BeatmapInfo,
+        mods: Mods,
+        stars: f32,
+        stats: ScoreStatistics,
+    ) -> anyhow::Result<()> {
+        writeln!(
+            writer,
+            "{map_id},{mods},{stars},{fc_count},{fc_flags}",
+            map_id = info.map.beatmap_id,
+            mods = mods.bits(),
+            stars = stars,
+            fc_count = stats.fc_count,
+            fc_flags = stats.fc_flags
+        )?;
         Ok(())
     }
 
@@ -427,5 +518,14 @@ impl Opts {
         );
 
         Ok(())
+    }
+
+    fn open_output_file(&self, name: &str) -> anyhow::Result<Box<dyn Write>> {
+        Ok(match &self.output_dir {
+            None => Box::new(std::io::stdout()),
+            Some(dir) => Box::new(BufWriter::new(
+                File::create(dir.join(name)).context("cannot open summary output file")?,
+            )),
+        })
     }
 }
